0e6ea1636e3a02b5345f73c8f9426d9ef1cc07c8
VanoKi <kirsvano@gmail.com>
2025-09-02 15:01:47 +0300

    feat: new set - todolists (2025-09-02 15:01:47)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {TodolistItem} from "./components/todolistItem.tsx";
+import {type filterType, TodolistItem} from "./components/todolistItem.tsx";
 import {nanoid} from "@reduxjs/toolkit";
 import {useState} from "react";
 
@@ -8,10 +8,20 @@ export type taskType = {
   title: string
   isDone: boolean
 }
+export type Todolist = {
+  id:string
+  title:string
+  filter:filterType
+}
 
 export const App = () => {
 
   const nanoid3 = () => nanoid(3)
+  const nanoid5 = () => nanoid(5)
+  const [todolist, setTodolists] = useState<Todolist[]>([
+    {id:nanoid5(), title: 'What to learn', filter: "All"},
+    {id:nanoid5(), title: 'What to do', filter: "All"}
+  ])
   const [tasks, setTasks] = useState<taskType[]>([
     { id: nanoid3(), title: 'HTML&CSS', isDone: true },
     { id: nanoid3(), title: 'JS', isDone: true },
