84cf1664350e0a351380729e7be91b2252aa19c2
VanoKi <kirsvano@gmail.com>
2025-09-04 17:04:04 +0300

    feat: add type to filter task todolist (2025-09-04 17:04:04)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 import './App.css'
+import {useState} from "react";
+import {nanoid} from "@reduxjs/toolkit";
+
+export type filterType = 'all' | 'active' | 'completed'
+export type Todolist ={
+  id:string
+  title:string
+  filter: filterType
+}
+export type taskType = {
+  id:string
+  title:string
+  isDone:boolean
+}
+
 
 export function App() {
+
+  const nanoid3 = () => nanoid(3)
+  const nanoid5 = () => nanoid(5)
+  const [todolist, setTodolists] = useState<Todolist[]>([
+    {id:nanoid5(), title: 'What to learn', filter: "All"},
+    {id:nanoid5(), title: 'What to do', filter: "All"}
+  ])
+  const [tasks, setTasks] = useState<taskType[]>([
+    { id: nanoid3(), title: 'HTML&CSS', isDone: true },
+    { id: nanoid3(), title: 'JS', isDone: true },
+    { id: nanoid3(), title: 'ReactJS', isDone: false },
+  ])
 
   return (
     <>
