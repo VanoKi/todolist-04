1b1ba96ee40215d1d29fe70b78c4e90c0d81b05f
VanoKi <kirsvano@gmail.com>
2025-09-05 16:19:31 +0300

    feat: change structure (2025-09-05 16:19:30)

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,24 @@ export const App = () => {
 
   const nanoid3 = () => nanoid(3)
   const nanoid5 = () => nanoid(5)
+
+  const todolistId1 = nanoid5()
+  const todolistId2 = nanoid5()
   const [todolists, setTodolists] = useState<Todolist[]>([
-    {id:nanoid5(), title: 'What to learn', filter: "All"},
-    {id:nanoid5(), title: 'What to do', filter: "All"}
+    {id:todolistId1, title: 'What to learn', filter: "All"},
+    {id:todolistId2, title: 'What to do', filter: "All"}
   ])
-  const [tasks, setTasks] = useState<taskType[]>([
-    { id: nanoid3(), title: 'HTML&CSS', isDone: true },
-    { id: nanoid3(), title: 'JS', isDone: true },
-    { id: nanoid3(), title: 'ReactJS', isDone: false },
-  ])
+  const [tasks, setTasks] = useState({
+    [todolistId1]:[
+      { id: nanoid3(), title: 'HTML&CSS', isDone: true },
+      { id: nanoid3(), title: 'JS', isDone: true },
+      { id: nanoid3(), title: 'ReactJS', isDone: false },
+    ],
+    [todolistId2]:[
+      { id: nanoid3(), title: 'Rest API', isDone: true },
+      { id: nanoid3(), title: 'GraphQL', isDone: false },
+    ]
+  })
 
   const deleteTask = (taskId:string) => {
     setTasks(tasks.filter(task => task.id !== taskId))
