4a1fbcdab19ba561117e1850a96c19197af0c872
VanoKi <kirsvano@gmail.com>
2025-09-09 17:10:17 +0300

    feat: create deleteTodolistAC (2025-09-09 17:10:15)

--- src/models/todolists-reducer.ts
+++ src/models/todolists-reducer.ts
@@ -1,3 +1,21 @@
-export const todolistsReducer = (state:any, action:any):any => {
+import type {Todolist} from "../App.tsx";
 
+const initialState: Todolist[] = []
+
+export const todolistsReducer = (state:Todolist[] = initialState, action:any):any => {
+  switch (action.type) {
+    case 'delete_todolist' : {
+      return state.filter(tl => tl.id !== action.payload.id)
+    }
+    default: return state
+  }
+}
+export type DeleteTodolistActtion = {
+  type: 'delete_todolist'
+  payload: {id:string}
+}
+type Actions = DeleteTodolistActtion
+
+export const deleteTodolistAC = (id:string): DeleteTodolistActtion => {
+  return {type: "delete_todolist", payload: {id}} as const
 }
