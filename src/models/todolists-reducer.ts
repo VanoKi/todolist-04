ba1beb118d7e27866ccbf29db3d2be0588c66a4a
VanoKi <kirsvano@gmail.com>
2025-09-10 19:04:26 +0300

    feat: add changeTodolistTitle (2025-09-10 19:04:25)

--- src/models/todolists-reducer.ts
+++ src/models/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import type {Todolist} from "../App.tsx";
+import {nanoid} from "@reduxjs/toolkit";
 
 const initialState: Todolist[] = []
 
@@ -7,6 +8,13 @@ export const todolistsReducer = (state:Todolist[] = initialState, action:Actions
     case 'delete_todolist' : {
       return state.filter(tl => tl.id !== action.payload.id)
     }
+    case 'create_todolist': {
+      const newTodolist = {id: nanoid(), title: action.payload.title, filter: 'All'}
+      return [newTodolist, ...state]
+    }
+    case 'change_todolist_title': {
+      return state.map( tl => tl.id === action.payload.todolistId ? {...tl, title: action.payload.title} : tl)
+    }
     default: return state
   }
 }
@@ -14,6 +22,14 @@ export const todolistsReducer = (state:Todolist[] = initialState, action:Actions
 export const deleteTodolistAC = (id:string): DeleteTodolistAction => {
   return {type: "delete_todolist", payload: {id}} as const
 }
+export const createTodolistAC = (title:string):CreateTodolistAction => {
+  return {type: 'create_todolist', payload: {title}}
+}
+export const changeTodolistTitleAC = (todolistId:string, title:string):ChangeTodolistTitleAction => {
+  return {type: 'change_todolist_title', payload: {todolistId, title}}
+}
 
 export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
-type Actions = DeleteTodolistAction
+export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
+export type ChangeTodolistTitleAction = ReturnType<typeof changeTodolistTitleAC>
+type Actions = DeleteTodolistAction | CreateTodolistAction | ChangeTodolistTitleAction
